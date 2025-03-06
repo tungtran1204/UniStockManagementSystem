28f54b9f438c995571baf5c905766882d3ea153a
phamnngoc <phamngoc1352005@gmail.com>
2025-03-06 15:00:41 +0700

    update font end receipt note

--- Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/receiptnote/ReceiptNoteController.java
+++ Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/receiptnote/ReceiptNoteController.java
@@ -1,4 +1,7 @@
 package vn.unistock.unistockmanagementsystem.features.user.receiptnote;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReceiptNoteController {
 }
