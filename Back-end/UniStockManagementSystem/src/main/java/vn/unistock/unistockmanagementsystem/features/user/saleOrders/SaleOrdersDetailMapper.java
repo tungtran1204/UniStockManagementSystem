e3473990a928fec73f843f10191ac2201e9d90a7
tungtran <tungtran120403@gmail.com>
2025-04-11 21:16:06 +0700

    Xuất kho trả hàng

--- Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/saleOrders/SaleOrdersDetailMapper.java
+++ Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/saleOrders/SaleOrdersDetailMapper.java
@@ -35,7 +35,8 @@ public interface SaleOrdersDetailMapper {
     @Mapping(source = "productId", target = "product.productId")
     @Mapping(source = "remainingQuantity", target = "remainingQuantity")
     @Mapping(source = "quantity", target = "quantity")
-    @Mapping(source = "receivedQuantity", target = "receivedQuantity")
+    @Mapping(target = "receivedQuantity", expression = "java(dto.getReceivedQuantity() == null ? 0 : dto.getReceivedQuantity())")
+
     @Mapping(source = "productCode",  target = "product.productCode")
     SalesOrderDetail toEntity(SalesOrderDetailDTO dto);
 
