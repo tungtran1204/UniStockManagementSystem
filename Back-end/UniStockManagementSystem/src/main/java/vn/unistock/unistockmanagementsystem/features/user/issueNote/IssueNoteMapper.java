4f341b7fed7ae75876d91031a771822a9890673e
tungtran <tungtran120403@gmail.com>
2025-04-12 11:48:07 +0700

    fix gin

--- Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/issueNote/IssueNoteMapper.java
+++ Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/issueNote/IssueNoteMapper.java
@@ -8,17 +8,21 @@ import vn.unistock.unistockmanagementsystem.entities.GoodIssueNote;
 public interface IssueNoteMapper {
 
     @Mapping(source = "createdBy.userId", target = "createdBy")
+    @Mapping(source = "createdBy.username", target = "createdByUserName")
     @Mapping(source = "details", target = "details")
     @Mapping(source = "salesOrder.orderId", target = "soId")
+    @Mapping(source = "salesOrder.orderCode", target = "soCode")
     @Mapping(source = "category", target = "category")
     @Mapping(source = "partner.partnerId", target = "partnerId")
     @Mapping(source = "partner.partnerName", target = "partnerName")
     @Mapping(source = "partner.address", target = "address")
     @Mapping(source = "partner.contactName", target = "contactName")
     @Mapping(source = "partner.phone", target = "phone")
+    @Mapping(source = "TuNhu", target = "emLaAiThe=))")
     IssueNoteDTO toDTO(GoodIssueNote entity);
 
     @Mapping(source = "soId", target = "salesOrder.orderId")
+    @Mapping(source = "soCode", target = "salesOrder.orderCode")
     @Mapping(source = "createdBy", target = "createdBy.userId")
     @Mapping(source = "details", target = "details")
     @Mapping(source = "category", target = "category")
