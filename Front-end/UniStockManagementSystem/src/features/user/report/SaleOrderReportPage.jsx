d3334b0fdb18a0fda6b4aae6e4938f4196190d27
phamnngoc <phamngoc1352005@gmail.com>
2025-04-16 22:29:53 +0700

    fe add ui filter for sale order page

--- Front-end/UniStockManagementSystem/src/features/user/report/SaleOrderReportPage.jsx
+++ Front-end/UniStockManagementSystem/src/features/user/report/SaleOrderReportPage.jsx
@@ -8,7 +8,6 @@ import TableSearch from '@/components/TableSearch';
 import Table from "@/components/Table";
 import QuantityFilterButton from "@/components/QuantityFilterButton";
 import StatusFilterButton from "@/components/StatusFilterButton";
-import DateFilterButton from "@/components/DateFilterButton";
 import dayjs from "dayjs";
 import "dayjs/locale/vi"; // Import Tiếng Việt
 
