47d308cd8025eb7df654b10863bf4b6927204f86
phamnngoc <phamngoc1352005@gmail.com>
2025-04-21 17:28:13 +0700

    fix

--- Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/materials/MaterialsRepository.java
+++ Back-end/UniStockManagementSystem/src/main/java/vn/unistock/unistockmanagementsystem/features/user/materials/MaterialsRepository.java
@@ -23,9 +23,6 @@ public interface MaterialsRepository extends JpaRepository<Material, Long> {
             "WHERE m.materialId = :materialId")
     Optional<Material> findByIdWithPartners(@Param("materialId") Long materialId);
 
-//    @Query("SELECT m FROM Material m JOIN m.materialPartners mp WHERE mp.partner.id = :partnerId")
-//    List<Material> findByPartnerId(@Param("partnerId") Long partnerId);
-
     @Query("SELECT m FROM Material m WHERE m.isUsing = true")
     List<Material> findAllByIsUsingTrue();
 
